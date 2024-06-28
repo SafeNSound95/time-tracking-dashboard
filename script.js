ae5e26e65d8878e6c5eeed3b9d185f9ec940e18d
Saif Shalan <safensound95@gmail.com>
2024-06-29 00:35:57 +0300

    added drop down functionality to all the ellipsis, changes in the html,css,js as a result.

--- script.js
+++ script.js
@@ -5,15 +5,40 @@ const allPrevTextElems = document.querySelectorAll(".previous-text");
 const profileActivityTimeElem = document.querySelector(
   ".profile-activity-time"
 );
+const allDropDownMenus = document.querySelectorAll(".dropdown-menu");
+
+const allEllipsis = document.querySelectorAll(".ellipsis");
+
+function removeMenu() {
+  allDropDownMenus.forEach((menu) => {
+    menu.classList.remove("dropdown-menu--active");
+  });
+}
+
+allEllipsis.forEach((ellipsis) =>
+  ellipsis.addEventListener("click", function () {
+    const currentDropDownMenu = ellipsis
+      .closest(".dropdown")
+      .querySelector(".dropdown-menu");
+
+    allDropDownMenus.forEach((menu) => {
+      if (menu === currentDropDownMenu) return;
+      menu.classList.remove("dropdown-menu--active");
+    });
+
+    currentDropDownMenu.classList.toggle("dropdown-menu--active");
+  })
+);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") removeMenu();
+});
+
+document.addEventListener("click", function (e) {
+  if (e.target === document.querySelector("body")) removeMenu();
+});
 
 const extractData = function (data, e) {
-  if (e.target === e.currentTarget) return;
-  [...document.querySelector(".profile-activity-time").children].forEach(
-    (spanEl) => spanEl.classList.remove("js-toggle")
-  );
-
-  e.target.classList.toggle("js-toggle");
-
   data.forEach((category, i) => {
     const timeFrame = e.target.textContent.toLowerCase();
     const timePrevKeyword =
@@ -33,18 +58,24 @@ const extractData = function (data, e) {
   });
 };
 
-profileActivityTimeElem.addEventListener("click", (e) =>
-  extractData(activityData, e)
-);
+profileActivityTimeElem.addEventListener("click", (e) => {
+  if (e.target === e.currentTarget) return;
+  [...document.querySelector(".profile-activity-time").children].forEach(
+    (spanEl) => spanEl.classList.remove("js-toggle")
+  );
 
-//Reading the local JSON file to get the data for the users' activities.
+  e.target.classList.toggle("js-toggle");
+  extractData(activityData, e);
+});
+
+allDropDownMenus.forEach((menu) =>
+  menu.addEventListener("click", function (e) {
+    if (e.target === e.currentTarget) return;
+    extractData(activityData, e);
+  })
+);
 
 let activityData;
 fetch("data.json")
   .then((response) => response.json())
   .then((data) => (activityData = data));
-
-/* 
-  1] Drop down menu (select) for epllipsis
-  2] Refactor
-*/
